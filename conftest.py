906b29b90e64fb6364928d332888396805edf5f3
Lucas Amaral <lopesdoamaral@gmail.com>
2024-12-05 17:23:39 -0300

    improve logging for debug

--- conftest.py
+++ conftest.py
@@ -1,14 +1,18 @@
+""" Pytest hooks and configurations"""
+import os
 import logging
 import pytest
 from playwright.sync_api import Page
 from utils.logger import logger
 
 
-log = logger(logging.DEBUG)
+log = logger(logging.INFO)
 
 
 @pytest.fixture(scope='function', autouse=True)
 def before_each(request, page: Page):
     """Actions take place before each test"""
+    log.info('openning browser on page: %s', os.getenv("BASE_URL"))
+    page.goto(os.getenv("BASE_URL"))
     log.info('running test: %s', request.node.name)
-    return page
+    return page, os.getenv("BASE_URL")
