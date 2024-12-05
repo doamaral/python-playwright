3731c7567556081043211d749e010ed1741e9463
Lucas Amaral <lopesdoamaral@gmail.com>
2024-12-05 17:24:11 -0300

    set locators and missing page element

--- pages/login.py
+++ pages/login.py
@@ -1,20 +1,20 @@
-
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
+""" Page object for Login page """
+import re
+from playwright.sync_api import Page
 
 
 class LoginPage:
     """Page Object that handles Login page"""
 
-    def __init__(self, page):
+    def __init__(self, page: Page):
         self.page = page
-        self.username_field = self.page.get_by_role("input", name="username")
-        self.password_field = self.page.get_by_role("input", name="password")
+        self.username_field = self.page.locator('[name="username"]')
+        self.password_field = self.page.locator('[name="password"]')
+        self.submit_button = self.page.get_by_role(
+            'button', name=re.compile("login", re.IGNORECASE))
 
     def do_login(self, username, password):
         """Method that handles all login process"""
-        self.page.goto(os.getenv("BASE_URL"))
         self.username_field.fill(username)
         self.password_field.fill(password)
+        self.submit_button.click()
